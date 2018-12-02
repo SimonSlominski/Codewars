a34edb02cac0817c475c2c5984d8a4cc32b72c3c
Szymon Słomiński <simon.slominski@gmail.com>
2018-12-02 10:59:47 +0100

    Add 8_kyu_Counting sheep

---
+++ 8_kyu/8_kyu_Counting sheep.py
@@ -0,0 +1,33 @@
+""" All tasks come from www.codewars.com """
+
+"""
+TASK: Counting sheep...
+
+Consider an array of sheep where some sheep may be missing from their place. 
+We need a function that counts the number of sheep present in the array (true means present).
+
+For example:
+
+[True,  True,  True,  False,
+  True,  True,  True,  True ,
+  True,  False, True,  False,
+  True,  False, False, True ,
+  True,  True,  True,  True ,
+  False, False, True,  True]
+
+The correct answer would be 17.
+
+Hint: Don't forget to check for bad values like null/undefined
+"""
+
+def count_sheeps(arrayOfSheeps):
+    return arrayOfSheeps.count(True)
+
+# And for newbie, for loop
+def count_sheeps(arrayOfSheeps):
+    lst = 0
+    for x in arrayOfSheeps:
+        if x == True:
+            lst += 1
+    return lst
+
