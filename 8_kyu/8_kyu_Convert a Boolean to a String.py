3a87366c2329c9cdbdc9b580c10fa8ff9d1f24a4
Szymon Słomiński <simon.slominski@gmail.com>
2019-02-09 16:59:40 +0100

    Add 8_kyu_Convert a Boolean to a String

---
+++ 8_kyu/8_kyu_Convert a Boolean to a String.py
@@ -0,0 +1,16 @@
+""" All tasks come from www.codewars.com """
+
+"""
+TASK: Convert a Boolean to a String
+
+In this example, the input to boolean_to_string (represented by the variable b) is a boolean.
+
+Lastly, when we want to take one action if a boolean is true, and another if it is false, we use an if-statement.
+
+For this kata, don't worry about edge cases like where unexpected input is passed to the function. 
+"""
+
+
+def boolean_to_string(b):
+    return str(b)
+
