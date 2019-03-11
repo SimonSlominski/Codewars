cdc4d0b0ca5248be89f4082db3a564ac8b4b97df
Szymon Słomiński <simon.slominski@gmail.com>
2019-03-11 09:19:37 +0100

    Add 7_kyu_Reverser

---
+++ 7_kyu/7_kyu_Reverser.py
@@ -0,0 +1,18 @@
+""" All tasks come from www.codewars.com """
+
+"""
+TASK: Reverser
+
+Impliment the reverse function, which takes in input n and reverses it. 
+For instance, reverse(123) should return 321. 
+You should do this without converting the inputted number into a string.
+"""
+
+
+def reverse(n):
+    rev = 0
+    while n:
+        rev = rev * 10 + n % 10
+        n = n // 10
+    return rev
+
