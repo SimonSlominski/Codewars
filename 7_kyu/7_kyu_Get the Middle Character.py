a6203d9bbc98f60a812954fb04014f215d40d3e2
Szymon Słomiński <simon.slominski@gmail.com>
2019-03-29 10:19:18 +0100

    Add 7_kyu_Get the Middle Character

---
+++ 7_kyu/7_kyu_Get the Middle Character.py
@@ -0,0 +1,24 @@
+""" All tasks come from www.codewars.com """
+
+"""
+TASK: Get the Middle Character
+
+You are going to be given a word. Your job is to return the middle character of the word. 
+If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+
+#Examples:
+
+Kata.getMiddle("test") should return "es"
+
+Kata.getMiddle("testing") should return "t"
+
+Kata.getMiddle("middle") should return "dd"
+
+Kata.getMiddle("A") should return "A"
+"""
+
+def middle_char(s):
+    if len(s) % 2 == 0:
+        return s[len(s)//2-1] + s[len(s)//2]
+    return s[len(s)//2]
+
