1350cf809f2d41b5bb1cbf115fb3d3949de4356a
Szymon Słomiński <simon.slominski@gmail.com>
2019-04-01 22:03:01 +0200

    Add 8_kyu_Even or Odd

---
+++ 8_kyu/8_kyu_Even or Odd.py
@@ -0,0 +1,13 @@
+""" All tasks come from www.codewars.com """
+
+"""
+TASK: Even or Odd
+
+Create a function (or write a script in Shell) that takes an integer as an argument and 
+returns "Even" for even numbers or "Odd" for odd numbers.
+"""
+
+
+def even_or_odd(num):
+    return 'Even' if num % 2 == 0 else 'Odd'
+
