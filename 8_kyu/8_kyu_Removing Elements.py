28c4a97ebe8f40b9190fd3324ae0dd7bebec100d
Szymon Słomiński <simon.slominski@gmail.com>
2019-06-18 22:49:11 +0200

    Add 8_kyu_Removing Elements

---
+++ 8_kyu/8_kyu_Removing Elements.py
@@ -0,0 +1,18 @@
+""" All tasks come from www.codewars.com """
+
+"""
+TASK: Removing Elements
+
+Take an array and remove every second element out of that array. 
+Always keep the first element and start removing with the next element.
+
+Example:
+my_list = ['Keep', 'Remove', 'Keep', 'Remove', 'Keep', ...]
+
+None of the arrays will be empty, so you don't have to worry about that!
+"""
+
+
+def remove_every_other(my_list):
+    return my_list[::2]
+
