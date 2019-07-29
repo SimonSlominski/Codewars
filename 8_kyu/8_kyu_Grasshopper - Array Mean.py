49f60e2b91452d2864ebea7a8c21a91a07a743f8
Szymon Słomiński <simon.slominski@gmail.com>
2019-07-29 09:10:03 +0200

    Add 8_kyu_Grasshopper - Array Mean

---
+++ 8_kyu/8_kyu_Grasshopper - Array Mean.py
@@ -0,0 +1,12 @@
+""" All tasks come from www.codewars.com """
+
+"""
+TASK: Grasshopper - Array Mean
+
+Find the mean (average) of a list of numbers in an array.
+"""
+
+
+def find_average(nums):
+    return sum(nums) / len(nums)
+
