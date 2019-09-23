b2b5756022f9b6273fc7a7cc1e477feefbc8e192
Szymon Słomiński <simon.slominski@gmail.com>
2019-09-23 10:46:41 +0200

    Add 6_kyu_Counting Duplicates

---
+++ 6_kyu/6_kyu_Counting Duplicates.py
@@ -0,0 +1,34 @@
+""" All tasks come from www.codewars.com """
+
+"""
+TASK: Counting Duplicates
+
+Count the number of Duplicates
+Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that 
+occur more than once in the input string. The input string can be assumed to contain only alphabets 
+(both uppercase and lowercase) and numeric digits.
+
+Example
+"abcde" -> 0 # no characters repeats more than once
+"aabbcde" -> 2 # 'a' and 'b'
+"aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+"indivisibility" -> 1 # 'i' occurs six times
+"Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+"aA11" -> 2 # 'a' and '1'
+"ABBA" -> 2 # 'A' and 'B' each occur twice
+"""
+
+
+from collections import Counter
+
+
+def duplicate_count(text):
+    characters = Counter(text.lower())
+    counter = 0
+
+    for x in characters.values():
+        if x >= 2:
+            counter += 1
+
+    return counter
+
