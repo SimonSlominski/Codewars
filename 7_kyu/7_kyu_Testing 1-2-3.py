c46f3969af993e516221ffa009d7f13a43b5e283
Szymon Słomiński <simon.slominski@gmail.com>
2019-12-02 12:25:48 +0100

    Add 7_kyu_Testing 1-2-3

---
+++ 7_kyu/7_kyu_Testing 1-2-3.py
@@ -0,0 +1,23 @@
+""" All tasks come from www.codewars.com """
+
+"""
+TASK: Testing 1-2-3
+
+Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+Examples:
+
+number([]) # => []
+number(["a", "b", "c"]) # => ["1: a", "2: b", "3: c"]
+"""
+
+
+def number(lines):
+    data = []
+    for index, value in enumerate(lines):
+        data.append("{}: {}".format(index+1, value))
+    return data
