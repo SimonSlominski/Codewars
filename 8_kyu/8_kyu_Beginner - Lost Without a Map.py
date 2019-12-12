d9506a144cab93adb8ad98a6750638916f2c0e18
Szymon Słomiński <simon.slominski@gmail.com>
2019-12-12 09:44:01 +0100

    Add 8_kyu_Beginner - Lost Without a Map

---
+++ 8_kyu/8_kyu_Beginner - Lost Without a Map.py
@@ -0,0 +1,14 @@
+""" All tasks come from www.codewars.com """
+
+"""
+TASK:  Beginner - Lost Without a Map
+
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+[1, 2, 3] --> [2, 4, 6]
+"""
+
+
+def maps(a):
+    return [num*2 for num in a]
