82bddeb0f9d52adcbd123a54b031837e1a379882
Szymon Słomiński <simon.slominski@gmail.com>
2020-01-21 11:53:01 +0100

    Add  8_kyu_Enumerable Magic - Take the First N Elements

---
+++ 8_kyu/8_kyu_Enumerable Magic #25 - Take the First N Elements.py
@@ -0,0 +1,11 @@
+""" All tasks come from www.codewars.com """
+
+"""
+TASK: Enumerable Magic #25 - Take the First N Elements
+
+Create a method take that accepts a list/array and a number n, 
+and returns a list/array array of the first n elements from the list/array.
+"""
+
+def take(arr,n):
+    return arr[:n]
