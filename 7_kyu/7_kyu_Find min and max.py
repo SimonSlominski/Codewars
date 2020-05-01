e2880c9b3e7daf461b0088527b60438b59e27d10
Szymon Słomiński <simon.slominski@gmail.com>
2020-05-01 18:10:29 +0200

    Add 7_kyu_Find min and max

---
+++ 7_kyu/7_kyu_Find min and max.py
@@ -0,0 +1,10 @@
+""" All tasks come from www.codewars.com """
+
+"""
+TASK: Find min and max
+
+Implement a function that returns the minimal and the maximal value of a list (in this order).
+"""
+
+def get_min_max(seq):
+    return min(seq), max(seq)
