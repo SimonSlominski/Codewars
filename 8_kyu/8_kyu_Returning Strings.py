3776d84e67fb4275ccefdee29964774a5c9a046b
Szymon Słomiński <simon.slominski@gmail.com>
2021-04-15 10:06:08 +0200

    Add 8_kyu_Returning Strings

---
+++ 8_kyu/8_kyu_Returning Strings.py
@@ -0,0 +1,11 @@
+""" All tasks come from www.codewars.com """
+
+"""
+TASK: Returning Strings
+
+Make a function that will return a greeting statement that uses an input; your program should return, 
+"Hello, <name> how are you doing today?"
+"""
+
+def greet(name):
+    return f"Hello, {name} how are you doing today?"
