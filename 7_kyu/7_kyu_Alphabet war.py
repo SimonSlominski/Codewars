7f312d3c1c4be405eb4b8b15c61265ba162b1520
Szymon Słomiński <simon.slominski@gmail.com>
2021-07-30 11:36:29 +0200

    Add 7_kyu_Alphabet war

---
+++ 7_kyu/7_kyu_Alphabet war.py
@@ -0,0 +1,60 @@
+""" All tasks come from www.codewars.com """
+
+"""
+TASK: Alphabet war
+
+Task
+Write a function that accepts fight string consists of only small letters and return who wins the fight. 
+When the left side wins return Left side wins!, when the right side wins return Right side wins!, 
+in other case return Let's fight again!.
+
+The left side letters and their power:
+ w - 4
+ p - 3
+ b - 2
+ s - 1
+
+The right side letters and their power:
+ m - 4
+ q - 3
+ d - 2
+ z - 1
+
+The other letters don't have power and are only victims.
+
+Example
+AlphabetWar("z");        //=> Right side wins!
+AlphabetWar("zdqmwpbs"); //=> Let's fight again!
+AlphabetWar("zzzzs");    //=> Right side wins!
+AlphabetWar("wwwwwwz");  //=> Left side wins!
+"""
+
+LEFT = {
+    "w": 4,
+    "p": 3,
+    "b": 2,
+    "s": 1,
+}
+
+RIGHT = {
+    "m": 4,
+    "q": 3,
+    "d": 2,
+    "z": 1,
+}
+
+def alphabet_war(fight):
+    left_score = sum([LEFT[letter]
+                      for letter in fight
+                      if letter in LEFT.keys()])
+
+    right_score = sum(RIGHT[letter]
+                      for letter in fight
+                      if letter in RIGHT.keys())
+
+    if left_score > right_score:
+        return f"Left side wins!"
+    elif left_score < right_score:
+        return f"Right side wins!"
+    else:
+        return f"Let's fight again!"
