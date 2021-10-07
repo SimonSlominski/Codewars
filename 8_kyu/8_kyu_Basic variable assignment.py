1cd2dbd1b214f2126fce7cc09c238a8c0c85c563
Szymon Słomiński <simon.slominski@gmail.com>
2021-10-07 11:53:39 +0200

    Add 8_kyu_Basic variable assignment

---
+++ 8_kyu/8_kyu_Basic variable assignment.py
@@ -0,0 +1,11 @@
+""" All tasks come from www.codewars.com """
+
+"""
+TASK: Basic variable assignment
+
+This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+"""
+
+a = "code"
+b = "wa.rs"
+name = a + b
