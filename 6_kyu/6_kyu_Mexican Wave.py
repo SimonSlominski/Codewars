e14ab9d3b37dbab3fbf7be22849ebef6f79e37f2
Szymon Słomiński <simon.slominski@gmail.com>
2022-02-18 17:31:44 +0100

    Add 6_kyu_Mexican Wave

---
+++ 6_kyu/6_kyu_Mexican Wave.py
@@ -0,0 +1,24 @@
+""" All tasks come from www.codewars.com """
+
+"""
+TASK: Mexican Wave
+
+Task
+In this simple Kata your task is to create a function that turns a string into a Mexican Wave. 
+You will be passed a string and you must return that string in an array where an uppercase letter is a person standing up. 
+
+Rules
+ 1.  The input string will always be lower case but maybe empty.
+ 2.  If the character in the string is whitespace then pass over it as if it was an empty seat
+ 
+ Example
+wave("hello") => ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
+"""
+
+
+def wave(people):
+    wave = []
+    for i in range(len(people)):
+        if people[i] != " ":
+            wave.append(people[:i] + people[i].upper() + people[i+1:])
+    return wave
