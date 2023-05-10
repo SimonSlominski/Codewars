f96189c5eb9d1afa1391f3da68b76f8c333f2f8d
Szymon Slominski <simon.slominski@gmail.com>
2023-05-10 12:56:45 +0200

    Add 8_kyu_String Templates - Bug Fixing #5

---
+++ 8_kyu/8_kyu_String Templates - Bug Fixing #5.py
@@ -0,0 +1,11 @@
+""" All tasks come from www.codewars.com """
+
+"""
+TASK: String Templates - Bug Fixing #5
+
+Oh no! Timmy hasn't followed instructions very carefully and forgot how to use the new String Template feature, 
+Help Timmy with his string template so it works as he expects!
+"""
+
+def build_string(*args):
+    return "I like {}!".format(", ".join(args))
