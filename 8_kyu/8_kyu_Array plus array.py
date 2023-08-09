11dbc7802559323631d5f845bb62d3898305b4a1
Szymon Slominski <simon.slominski@gmail.com>
2023-08-09 21:20:18 +0200

    Add 8_kyu_Array plus array

---
+++ 8_kyu/8_kyu_Array plus array.py
@@ -0,0 +1,13 @@
+""" All tasks come from www.codewars.com """
+
+"""
+TASK: Array plus array
+
+Get the sum of two arrays... Actually the sum of all their elements.
+
+P.S. Each array includes only integer numbers. Output is a number too.
+"""
+
+
+def array_plus_array(arr1,arr2):
+    return sum(arr1 + arr2)
